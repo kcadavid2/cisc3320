b1abf8c3db7be78cc62c49f4c2d93b92e75f2887
jamaalg <jamaal1087@gmail.com>
2016-12-16 16:01:51 -0500

    updated placeJob() function

--- MemoryManager.h
+++ MemoryManager.h
@@ -63,47 +63,81 @@ void printMemoryTable(){
 }
     
     
-/*    
-    
-////// ------------------ currently working on
-// Place job into memory
+
+// Places Job Into Memory
 void placeJob(Job &j){
-	// Divides job size by 1000 to find how many memory chunks it will use
-	int x = j.jobSize /1000;
-    int counter = 0;
-    
-	// Test if job will fit into memory
-	if (jobFit(j)){
+
+	int counter =0;
+
+    // Store size and jobNumber
+	int sizeOfJob = j.jobSize;
+	int jId = j.jobNumber;
+
+    // Pointer to beginning of jobs array
+	int *n = (int*)&arr[0];
+
+	for (int i=0; i<arrSize-sizeOfJob; i++){
+
+		counter =0;
         
-		for (int i=0; i<memoryChunk; i++){
-			if (arrayOfJobs[i] == 0 && x !=0){
-				
-                arrayOfJobs[i] = j.jobNumber;
-				
-                j.joblocations.insert(i);
-				
-                x--;
+        // If current array position doesnt have jobNumber
+		if (arr[i] == 0){
+            
+            
+			n = (int*)&arr[i];
+            
+            // Loop to find contiguous memory slots
+			while (*n==0 && counter!=sizeOfJob){
+				counter++;
+				n++;
 			}
-		}
-		set<int>::iterator it;
+            
+            
+            // If there is enough contiguous memory, place job into array
+			if (counter == sizeOfJob){
+                
+                    std::cout << "Job H"<< j.jobNumber << " may fit, beginning with index " << i << " and ending at " << i + (counter-1) <<endl;
 
-		cout << "Job " << j.id << " placed into memory at locations: ";
-		for (it =j.joblocations.begin() ; it != j.joblocations.end();it++){
-				cout << *it << " ";
+                    // Set memory location of job
+                    j.memoryLocation=i;
 
-		}
+                    // Set pointer to first memory address locaction  of job
+                    n = (int*)&arr[i];
 
-			cout << endl << getFreeMemory(arr,arrSize) <<endl;
-		} else {
-			cout << "Could not place job into memory!" <<endl;
+                    // Places jobNumber into contiguous memory slots
+                    for (int j=i-1; j <(i+(counter-1));j++){
+                        *n = jId;
+                        n++;
+                    }
+
+                    // Set pointer to null
+                    n=NULL;
+
+                    // Print memory Table
+                    printMemoryTable();
+                
+                    j.inMem =true;
+                    break;
+			
+                }  else   {
+                    
+                    // Go to next 
+                    continue;
+			     }
+		      } else {
+			         std::cout << "Job with id: " << j.id << " cannot be placed into memory @ " << i <<endl;
+			continue;
 		}
+	}
+    
+    // Set pointer to null
+    n=NULL;
 }
 
 
-
-
-*/
-
+// There is a small error int this code 
+// Jobs 
+    
 // Removes job from memory 
 void removeJob(Job &j){
     
@@ -137,22 +171,7 @@ void removeJob(Job &j){
     j.inMem = false;
 
 }
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+   
     
 };
 
