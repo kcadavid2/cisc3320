75435a54e9cb120a95d6b96c56ae44266a50526a
sunnyshapir <sunnyshapir@gmail.com>
2016-12-19 10:43:16 -0500

    Changed return data type

--- cpuScheduler.h
+++ cpuScheduler.h
@@ -7,16 +7,8 @@ using namespace std;
 
 class cpuScheduler {
 	public:
-/*		long scheduleCpu (vector<Job> jobTable) {
-			//First Come, First Served
-			for (long i = 0; i < jobTable.size(); i++) {
-				if (jobTable[i].getInMem() && !jobTable[i].getDoingIo())
-					return i;
-			}
-			return -1;
-		}*/
 	//STS: First Come, First Served using ready queue
-		long scheduleCpu (queue<Job> readyQueue) {
+		Job scheduleCpu (queue<Job> readyQueue) {
 			queue<Job> ready = readyQueue;
 			Job toRun = NULL;
 			while (!ready.empty()) {
@@ -29,8 +21,6 @@ class cpuScheduler {
 			}
 			return toRun;
 		}
-
-		//LTS: Swapper
 };
 
 #endif /* CPUSCHEDULER_H_ */
